e699bb8d2d42e69867b01ce0ad673e7c72502436
mohamed ahmed fathelbab <fathelbabmohamed94@gmail.com>
2022-04-22 13:39:07 +0200

    Fixed some validation conditions

--- src/routes/api/getImage.ts
+++ src/routes/api/getImage.ts
@@ -1,9 +1,8 @@
 import express from 'express';
-import {Request, Response,NextFunction} from 'express';
-import fs from 'fs'; 
+import {Request, Response} from 'express';
 import  sharp from 'sharp';
 import path from 'path';
-import { promises as fsPromises } from 'fs';
+import { promises as fsPromise } from 'fs';
 const image = express.Router();
 
 const resizeImage = async  (
@@ -16,34 +15,37 @@ const resizeImage = async  (
   console.log('=====>',outputFolderPath)
 
   try {
-    await fsPromises.access(outputFolderPath);
+    await fsPromise.access(outputFolderPath);
   } catch (error) {
     await sharp(imageFolderPath).resize(width, height).toFile(outputFolderPath);
   }
   return outputFolderPath;
 }
 image.get('/', async (req: Request, res: Response): Promise<void> => {
-    const filename = req.query.filename as string | undefined;
     const width = parseInt(req.query.width as string);
+    const filename = req.query.filename as string | undefined;
     const height = parseInt(req.query.height as string);
   
+    // check if the file name exists
     if (!filename) {
       res.status(400).send( 'you are missing the photo name' );
       return;
     }
-    if (!(width >= 0)) {
+    // check if the width is valid
+    if (!width || width === 0) {
       res
         .status(400)
         .send('please type a valid width' );
       return;
     }
-    if (!(height >= 0)) {
+    //    check if the height is valid
+     if (!height || height === 0) {
       res
         .status(400)
         .send('please type a valid height' );
       return;
     }
-  
+    // start resizing
     try {
       const outputThumb = await resizeImage(width, height, filename);
       console.log('finding image')
