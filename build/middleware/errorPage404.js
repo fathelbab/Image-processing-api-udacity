d38dfc0d7e922187766366b56b0c24042279c7d4
mohamed ahmed fathelbab <fathelbabmohamed94@gmail.com>
2022-04-22 14:46:42 +0200

    Fix: removed build folder

--- build/middleware/errorPage404.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorPage = void 0;
-const errorPage = (req, res) => {
-    res.status(404).send('404 Not Found');
-};
-exports.errorPage = errorPage;
--- build/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loggerMiddleware = void 0;
-const morgan_1 = __importDefault(require("morgan"));
-exports.loggerMiddleware = (0, morgan_1.default)('common');
